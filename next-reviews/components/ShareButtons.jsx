6549fb8f0ed7545d5b6001fa9c3ff7af04b3aa3a
nikola.ninov <momzzze@abv.bg>
2023-10-08 13:20:08 +0300

    just add fresh copy of the reviews project

--- next-reviews/components/ShareButtons.jsx
+++ next-reviews/components/ShareButtons.jsx
@@ -4,7 +4,7 @@ const ShareButtons = () => {
     console.log('[ShareButton] rendering');
     return (
         <div>
-            <ShareLinkButton />    
+            <ShareLinkButton />     
         </div>
     )
 }
