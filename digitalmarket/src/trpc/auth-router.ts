72015c47aa3234b134a8fda6e28216f694750ed4
nikola.ninov <momzzze@abv.bg>
2023-11-30 16:43:37 +0200

    verification email works
    I don't have verified domain and will work only for my email

--- digitalmarket/src/trpc/auth-router.ts
+++ digitalmarket/src/trpc/auth-router.ts
@@ -5,32 +5,34 @@ import { TRPCError } from "@trpc/server";
 import { AuthCredentialsValidator } from "../lib/validators/account-credentials-validator";
 
 export const authRouter = router({
-    createPayloadUser: publicProcedure.input(AuthCredentialsValidator)
-        .mutation(async ({ input }) => {
-            const { email, password } = input;
-            const payload = await getPayloadClient();
+    createPayloadUser: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input }) => {
+      const { email, password } = input
+      const payload = await getPayloadClient()
 
-            const { docs: users } = await payload.find({
-                collection: 'users',
-                where: {
-                    email: {
-                        equals: email,
-                    }
-                }
-            })
+      // check if user already exists
+      const { docs: users } = await payload.find({
+        collection: 'users',
+        where: {
+          email: {
+            equals: email,
+          },
+        },
+      })
 
-            if (users.length !== 0){
-                throw new TRPCError({code: 'CONFLICT'})
-            }
+      if (users.length !== 0)
+        throw new TRPCError({ code: 'CONFLICT' })
 
-            await payload.create({
-                collection: 'users',
-                data: {
-                    email,
-                    password,
-                    role: 'user',
-                }       
-            })
-            return {success: true, sentToEmail: email}
-    })
+      await payload.create({
+        collection: 'users',
+        data: {
+          email,
+          password,
+          role: 'user',
+        },
+      })
+
+      return { success: true, sentToEmail: email }
+    }),
 })
