3f07af394eb98ebce95e3600bd4f7faec6c484d7
nikola.ninov <momzzze@abv.bg>
2023-12-01 18:21:38 +0200

    finish drag&drop and display added files on the screen in the dashboard

--- dropbox-clone/app/dashboard/page.tsx
+++ dropbox-clone/app/dashboard/page.tsx
@@ -4,17 +4,38 @@ import Dropzone from "@/components/Dropzone";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "@/firebase";
 import { FileType } from "@/typings";
+import TableWrapper from "@/components/table/TableWrapper";
 
 async function Dashboard() {
   const { userId } = auth();
 
   const docsResults = await getDocs(collection(db, "users", userId!, "files"));
-  const sceletonFiles: FileType[]=[];
+  // implicit return of object
+  const sceletonFiles: FileType[] = docsResults.docs.map((doc) => ({
+    id: doc.id, 
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadUrl: doc.data().downloadUrl,   
+    size: doc.data().size,
+    type: doc.data().type,
+  }));
+
+
+  
 
   return (
-    <>
+    <div className="border-t">
       <Dropzone />
-    </>
+      <section className="container space-y-5">
+        <h2 className="font-bold">All Files</h2>
+
+        <div className="">
+            {/* table wrapper */}
+            <TableWrapper sceletonFiles={sceletonFiles}/>
+        </div>
+      </section>
+    </div>
   );
 }
 
