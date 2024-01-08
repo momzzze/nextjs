aa5925b71503980dc9adbabe00790db6d4709a0f
nikola.ninov <momzzze@abv.bg>
2024-01-08 12:21:40 +0200

    add sidebar, add zustand for state managment,

---
+++ twitch-clone/app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Wrapper } from "./wrapper";
+import Toggle from "./toggle";
+
+const Sidebar = () => {
+  //fetch followed
+  //fetch recommended
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
+};
+
+export default Sidebar;
